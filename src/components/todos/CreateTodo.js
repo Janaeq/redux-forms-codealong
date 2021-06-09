a7e7017bec827a062ffa1daa4533a39f471d2afd
Janae Quinones <janaemassa@gmail.com>
2021-06-09 19:25:34 -0400

    commit tree 2ea183b33f9cf8d252829d89362a1863d45e730a

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
+// import connect to connect createTodo to the application store
+import { connect } from 'react-redux'
 
 class CreateTodo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: ''
+    }
+  }
+
+  // updates the component's state each time the user types something into the input field
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault() 
+    this.props.addTodo(this.state)
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form>
+          <p>
+            <label>add todo</label>
+            {/* set the input value to the component state so that every key stroke will update the state */}
+            <input type='text' onChange={this.handleChange} value={this.state.text} />
+          </p>
+          <input type='submit' onSubmit={this.handleSubmit} />
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+const mapDispatchToProps = dispatch => {
+  // sends the value of the user input that was captured in the local component state to the redux store
+  return {
+    // include the data from the form as the key called payload
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  }
+}
+// this component only dispatch an action. no need to map state to props.
+// in it's place, use null
+export default connect(null, mapDispatchToProps)(CreateTodo);
